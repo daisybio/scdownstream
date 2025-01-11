9fde2e0e789bedbeade973d5e8ae23662a4cf2a2
Nico Trummer <nictru32@gmail.com>
2025-01-11 15:20:50 +0100

    Require seqera platform token for scvi GPU image build

--- modules/local/scvitools/gpu_container/build.sh
+++ modules/local/scvitools/gpu_container/build.sh
@@ -3,7 +3,14 @@
 # Prerequisite:
 # Install wave: https://docs.seqera.io/wave/cli/reference#install-the-wave-cli
 
-CONTAINER_URL="$(wave -f Dockerfile --context .)"
+PLATFORM_TOKEN=$1
+
+if [ -z "$PLATFORM_TOKEN" ]; then
+    echo "Please provide a platform token, as the build time exceeds the 15 minutes in the free tier"
+    exit
+fi
+
+CONTAINER_URL="$(wave -f Dockerfile --context . --tower-token $PLATFORM_TOKEN)"
 
 # If docker is installed
 
